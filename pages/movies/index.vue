be7d3f2b69c1477a105e4476878b7e1081c040e7
DESKTOP-DCLTBK8\rothw <rothwell.andy@gmail.com>
2024-05-25 11:03:17 -0400

    makes movie search and clicking work

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,9 +1,26 @@
-<script lang="ts" setup></script>
+<script setup>
+  const query = ref("");
+  const movies = ref([]);
+  async function search() {
+    const {Search} = await $fetch(`https://www.omdbapi.com/?apikey=8e3f600b&s=${query.value}`)
+    movies.value = Search; 
+  }
+</script>
 
 <template>
-  <div>
-    Page: movies
-  </div>
+  <!-- <div> -->
+    <form @submit.prevent="search">
+      <input type="text" v-model="query">
+      <button>Search</button>
+    </form>
+    <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
+      <li v-for="movie in movies" :key="movies.imdbID">
+        <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+          <img :src="movie.Poster" :alt="movies.title" />
+        </NuxtLink>
+      </li>
+    </ul>
+  <!-- </div> -->
 </template>
 
 <style scoped></style>
